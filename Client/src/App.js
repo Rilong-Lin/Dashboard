5cfbbe93f22acb84ab2640c6f9bfbf41869edb75
Rilong LIN <rilong.lin@epitech.eu>
2022-02-02 15:12:25 +0100

    [ADD] dashboard project

---
+++ Client/src/App.js
@@ -0,0 +1,20 @@
+import Home from './pages/Home'
+import PageLogin from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageRegister from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" caseSensitive={false} element={<PageLogin />} />
+          <Route path="/PageRegister" caseSensitive={false} element={<PageRegister />} />
+          <Route path="/Home" caseSensitive={false} element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
